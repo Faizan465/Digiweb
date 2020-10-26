50321b17a3140e31f047806b8e3f305b5bcd7917
faizan <mf496860@gmail.com>
2020-10-26 22:22:48 +0530

    push

--- src/component/portfolios.jsx
+++ src/component/portfolios.jsx
@@ -6,7 +6,7 @@ class Portfolios  extends Component {
         return ( 
             <div>
                 <section className="hero-wrap hero-wrap-2" style={{backgroundImage: 'url(images/bg_3.jpg)'}} data-stellar-background-ratio="0.5">
-                <div className="overlay"></div>
+             
  <div className="container">
     <div className="row no-gutters slider-text align-items-end justify-content-center">
       <div className="col-md-9 ftco-animate pb-5 text-center">
--- src/component/services.jsx
+++ src/component/services.jsx
@@ -116,7 +116,7 @@ been able to provide best services for our customers.</p>
 								<h3 style={{color:'rgba(31,181,172,.9)'}}> Designing</h3>
 							</p>
 					<p>
-               We create designs that are attractive and functional. After all, the website represents you and your business on the internet. 
+               We create designs that are attractive and functional. After all, the website represents  on the internet . 
 					</p>
 				</div>
 			</div>
@@ -130,7 +130,7 @@ been able to provide best services for our customers.</p>
                             <h3 style={{color:'rgba(31,181,172,.9)'}}>Crazy Coding</h3>
 						</p>
 					<p>
-               We strive to create the best digital content out there for all platforms such as the web, mobile, tablets, digital screens.
+               We strive to create the best digital content out there for all platforms such as the web, mobile, tablets
 					</p>
 				</div>
 			</div>
@@ -143,7 +143,7 @@ been able to provide best services for our customers.</p>
 							<h3 style={{color:'rgba(31,181,172,.9)'}} >We're Passionate</h3>
 						</p>
 					<p>
-               We believe to produce top notch quality work which please our clients and help them to meet their business goals.
+               We believe to produce top notch quality work which please our clients and help them to meet their  goals.
 					</p>
 				</div>
 			</div>
@@ -169,7 +169,7 @@ been able to provide best services for our customers.</p>
 							<h3 style={{color:'rgba(31,181,172,.9)'}}>Branding</h3>
 						</p>
 					<p>
-               We work with the world's top Development agencies to create flawless Design contents with super-slick .
+               We work with the world's top Development agencies to create flawless Design contents with super-slick  at digiweb.
 					</p>
 				</div>
 			</div>
@@ -185,7 +185,7 @@ been able to provide best services for our customers.</p>
 
                      
 						<p>
-                  If you can imagine it, we can build it. We use lightweight code libraries, tricks, hacks, secret work-arounds.
+                  If you can imagine it, we can build it. We use lightweight code libraries, tricks, hacks, secret  
 						</p>
 				</div>
 			</div>
@@ -198,7 +198,7 @@ been able to provide best services for our customers.</p>
 							<h3 style={{color:'rgba(31,181,172,.9)'}}> Experts</h3>
 						</p>
 					<p>
-					With an experience of more than 5+ years,Having experts on Website design, Development , E-Commerce .
+					With an experience of more than 5+ years,Having experts on Website design, Development , E-Commerce  etc.
 					</p>
 				</div>
 			</div>
@@ -220,19 +220,9 @@ been able to provide best services for our customers.</p>
 	</div>
 </div>
 
-<br/>
-<br/>
 <div className="center"></div>
                  <center >    <img src="http://www.flyaway.co.in/images/web-design.png" className="img-responsive"/></center>
                       <div className="space"></div>
-                      
-           
-       
-
-
-
-<br/>
-<br/>
 	<center>
         <div className="col-lg-4"> 
 		
@@ -260,67 +250,7 @@ been able to provide best services for our customers.</p>
 		<br/>
 		<br/>
 		</center>
-		<footer className="ftco-footer ftco-footer-2 ftco-section"  >
-			<div className="container">
-				<div className="row mb-5">
-					<div className="col-md">
-						<div className="ftco-footer-widget mb-4">
-							<h2 className="ftco-footer-logo"><img src="images/imges.png" style={{height:'40px',width:'60px',marginLeft:'10%',zIndex:'0',backgroundColor:'#05091f'}}/></h2>
-							<ul className="ftco-footer-social list-unstyled mt-2">
-								<li className="ftco-animate"><a href="https://twitter.com/home"><span className="fa fa-twitter"></span></a></li>
-								<li className="ftco-animate"><a href="https://www.facebook.com/DigiWebco/"><span className="fa fa-facebook"></span></a></li>
-								<li className="ftco-animate"><a href="https://github.com/Faizan465"><span className="fa fa-github"></span></a></li>
-							</ul>
-						</div>
-					</div>
-					<div className="col-md">
-						<div className="ftco-footer-widget mb-4 ml-md-5">
-							<h2 className="ftco-heading-2">Explore</h2>
-							<ul className="list-unstyled">
-								<li><Link to="/about" className="py-2 d-block">About</Link></li>
-								<li><Link to="/contact" className="py-2 d-block">Contact</Link></li>
-								<li><Link to="/services" className="py-2 d-block">What We Do</Link></li>
-								
-								
-								<li><Link to="" className="py-2 d-block">Call Us</Link></li>
-							</ul>
-						</div>
-					</div>
-					<div className="col-md">
-						<div className="ftco-footer-widget mb-4">
-							<h2 className="ftco-heading-2">Legal</h2>
-							<ul className="list-unstyled">
-							
-								<li><Link to="/blog" className="py-2 d-block">Blog</Link></li>
-								<li><Link to="/home" className="py-2 d-block">Privacy &amp; Policy</Link></li>
-								<li><Link to="/home" className="py-2 d-block">Terms &amp; Conditions</Link></li>
-								
-								<li><Link to="/contact" className="py-2 d-block">Contact</Link></li>
-							</ul>
-						</div>
-					</div>
-					<div className="col-md">
-						<div className="ftco-footer-widget mb-4">
-							<h2 className="ftco-heading-2">Have a Questions?</h2>
-							<div className="block-23 mb-3">
-								<ul>
-									<li><span className="icon fa fa-map marker"></span><span className="text"> Sigra Varanasi</span></li>
-									<li> <Link to="tel:8383049524"><span className="icon fa fa-phone"></span><span className="text">+91 8383049524</span></Link></li>
-									<li><Link to="mailto:contact@digiwebsolutions.tech"><span className="icon fa fa-paper-plane pr-4"></span><span className="text">contact@digiwebsolutions.tech</span></Link></li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-md-12 text-center">
-						<p>
-							Copyright & copy;<script>document.write(new Date().getFullYear());</script> All rights reserved |Developed <i className="fa fa-heart" aria-hidden="true"></i>by  <Link to="" target="_blank">DigiWeb Solution</Link>
-							</p>
-						</div>
-					</div>
-				</div>
-			</footer> 
+		
  	</div>
 	 </div>
 	
